620b78e1a252529bd8b2741ced292cfbf23dd729
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-08-23 16:34:19 +0300

    plus form, game counter, without styles and score table

--- src/App.js
+++ src/App.js
@@ -1,17 +1,83 @@
 /* eslint-disable react/no-array-index-key */
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import Square from './components/Square/Square';
+import React, { useState, useEffect, useCallback } from 'react';
+import Field from './components/Field/Field'
 import Modal from './components/Modal/Modal';
 import Patterns from './patterns/Patterns';
-import { PlayersContext } from './components/PlayersContext/PlayersContext';
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(''));
   const [player, setPlayer] = useState('O');
   const [result, setResult] = useState({ winner: 'none', state: 'none' });
   const [modalActive, setModalActive] = useState(true);
-  const [players, setPlayers] = useState(['', '']);
+  const [name1, setName1] = useState('');
+  const [name2, setName2] = useState('');
+  const [players, setPlayers] = useState([]);
+  const [games, setGames] = useState(0);
+
+  const gamesIncrease = useCallback(() => {
+    setGames(games + 1);
+  }, [result]);
+
+  // const changeScore = () => {
+  //   if (games % 2 === 0) {
+  //     setPlayers();
+  //   }
+
+  //   setPlayers([...players, players[1].wins + 1]);
+  // }
+
+  const chooseField = (field) => {
+    setBoard(board.map((value, index) => {
+      if (index === field && value === '') {
+        return player;
+      }
+
+      return value;
+    }));
+  };
+
+  const checkWin = useCallback(() => {
+    Patterns.forEach((currPattern) => {
+      const firstPlayer = board[currPattern[0]];
+
+      if (firstPlayer === '') {
+        return;
+      }
+
+      let foundWinningPattern = true;
+
+      currPattern.forEach((index) => {
+        if (board[index] !== firstPlayer) {
+          foundWinningPattern = false;
+        }
+      });
+      if (foundWinningPattern) {
+        setResult({ winner: player, state: 'won' });
+        console.log(`${player} won!`);
+        gamesIncrease();
+        // changeScore();
+      }
+    });
+  }, [board, player]);
+
+  const checkIfTie = useCallback(() => {
+    let filled = true;
+
+    board.forEach((field) => {
+      if (field === '') {
+        filled = false;
+      }
+    });
+    if (filled) {
+      setResult({ winner: 'No One', state: 'Tie' });
+      gamesIncrease();
+    }
+  }, [board]);
+
+  const restartGame = () => {
+    setBoard(Array(9).fill(''));
+  };
 
   useEffect(() => {
     checkWin();
@@ -29,68 +95,35 @@ function App() {
     }
   }, [result]);
 
-  const chooseSquare = (square) => {
-    setBoard(board.map((value, index) => {
-      if (index === square && value === '') {
-        return player;
-      }
+  const submitPlayers = (event) => {
+    event.preventDefault();
+    const player1 = {
+      name: name1,
+      wins: 0,
+    };
 
-      return value;
-    }));
-  };
+    const player2 = {
+      name: name2,
+      wins: 0,
+    };
 
-  const checkWin = () => {
-    Patterns.forEach((currPattern) => {
-      const firstPlayer = board[currPattern[0]];
-
-      if (firstPlayer === '') return;
-      let foundWinningPattern = true;
-
-      currPattern.forEach((index) => {
-        if (board[index] !== firstPlayer) {
-          foundWinningPattern = false;
-          console.log(`pattern is not found`);
-        }
-      });
-      if (foundWinningPattern) {
-        setResult({ winner: player, state: 'won' });
-        console.log(`${player} won!`);
-      }
-    });
-  };
-
-  const checkIfTie = () => {
-    let filled = true;
-
-    board.forEach((square) => {
-      if (square === '') {
-        filled = false;
-      }
-    });
-    if (filled) {
-      setResult({ winner: 'No One', state: 'Tie' });
-      console.log('Tie!');
-    }
-  };
-
-  const restartGame = () => {
-    setBoard(Array(9).fill(''));
+    setPlayers(() => [player1, player2]);
+    setName1('');
+    setName2('');
+    setModalActive(false);
+    console.log('non-active');
   };
 
   return (
-    <PlayersContext.Provider
-      value={players}
-      setPlayers={setPlayers}
-    >
     <div className="App">
       <div className="frame">
         <div className="board">
           {board.map((v, i, ar) => (
-            <Square
+            <Field
               key={i}
               value={board[i]}
-              chooseSquare={() => {
-                chooseSquare(i);
+              chooseField={() => {
+                chooseField(i);
               }}
             />
           ))
@@ -99,6 +132,17 @@ function App() {
       </div>
 
       <div className="frame">
+        <p>
+          Score
+        </p>
+        <p>
+          {`${players.length && players[0].name}:
+          ${players.length && players[0].wins}`}
+        </p>
+        <p>
+          {`${players.length && players[1].name}:
+          ${players.length && players[1].wins}`}
+        </p>
         <button
           className="button"
           type="button"
@@ -111,12 +155,19 @@ function App() {
           type="button"
           onClick={() => setModalActive(true)}
         >
-          Input new players
+          Set new players
         </button>
       </div>
-      <Modal active={modalActive} setActive={setModalActive} />
+      <Modal
+        active={modalActive}
+        setActive={setModalActive}
+        submitPlayers={submitPlayers}
+        name1={name1}
+        name2={name2}
+        setName1={setName1}
+        setName2={setName2}
+      />
     </div>
-    </PlayersContext.Provider>
   );
 }
 
